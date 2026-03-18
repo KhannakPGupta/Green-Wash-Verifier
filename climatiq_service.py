13e4294950365012bb72d4ab71f064cad248bff0
KhannakPGupta <khannakpgupta@hotmail.com>
2026-03-18 20:14:17 +0530

    Restore safe fallbacks to prevent UI crash heavily

--- climatiq_service.py
+++ climatiq_service.py
@@ -12,24 +12,40 @@ def get_transport_emissions(mode,distance,weight):
         "Air Freight": "freight_plane"
     }
 
-    url = "https://api.climatiq.io/data/v1/estimate"
-    headers={"Authorization":f"Bearer {CLIMATIQ_KEY}"}   #Bearer Token Authentication
-
-    #The data
-    payload = {
-        "emission_factor": {
-            "activity_id": mode_map[mode],
-            "data_version": "^7"    # ^ symbol tells API to use latest dynamic version of emission data
-        },
-        "parameters": {
-            "distance": distance,
-            "distance_unit": "km",
-            "weight": weight,
-            "weight_unit": "kg"
-        }
-    }
+    fallback = {
+            "Rail": 0.02,
+            "Cargo Ship": 0.015,
+            "Diesel Truck": 0.1,
+            "Electric Truck": 0.05,
+            "Air Freight": 0.5
+        }    
     
-    response = requests.post(url, json=payload, headers=headers, timeout=5)
-    response.raise_for_status()
-    result = response.json()
-    return result["co2e"]       #Extracts and returns CO2 equivalent in kg 
+    try:
+        url = "https://api.climatiq.io/data/v1/estimate"
+        headers={"Authorization":f"Bearer {CLIMATIQ_KEY}"}   #Bearer Token Authentication
+
+        #The data
+        payload = {
+            "emission_factor": {
+                "activity_id": mode_map[mode],
+                "data_version": "^7"    # ^ symbol tells API to use latest dynamic version of emission data
+            },
+            "parameters": {
+                "distance": distance,
+                "distance_unit": "km",
+                "weight": weight,
+                "weight_unit": "kg"
+            }
+        }
+        
+        response = requests.post(url, json=payload, headers=headers, timeout=5)
+        response.raise_for_status()
+        result = response.json()
+        return result["co2e"]       #Extracts and returns CO2 equivalent in kg 
+
+    except requests.exceptions.Timeout:
+        return fallback[mode]*distance*weight
+    except requests.exceptions.ConnectionError:
+        return fallback[mode]*distance*weight
+    except Exception:
+        return fallback[mode]*distance*weight
